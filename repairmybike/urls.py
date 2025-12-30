49dd61e317fcaafd78183ce000e87f30b362ad49
mahir <mahir286sharma@gmail.com>
2025-12-31 00:40:59 +0530

    update urls for push

--- repairmybike/urls.py
+++ repairmybike/urls.py
@@ -19,6 +19,5 @@ urlpatterns = [
     path('api/subscriptions/', include('subscriptions.urls')),
 ]
 
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files in development and production (fallback)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
