d28fbf32b02868ee201675c98e0a955d0cce0cd8
mahir <mahir286sharma@gmail.com>
2026-01-03 14:43:11 +0530

    new origin

--- repairmybike/settings.py
+++ repairmybike/settings.py
@@ -285,7 +285,7 @@ REST_FRAMEWORK = {
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:3000,http://127.0.0.1:3000,https://repairmybikebackend-production.up.railway.app',
+    default='http://localhost:3000,http://127.0.0.1:3000,https://repairmybikebackend-production.up.railway.app','https://repairmybike-frontend.vercel.app',
     cast=lambda v: [s.strip() for s in v.split(',')]
 )
 
