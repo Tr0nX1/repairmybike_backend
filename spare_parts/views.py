864c4c03f9f65e0d6d993a9cbeb96b2b0c2c09b7
mahir <mahir286sharma@gmail.com>
2026-01-04 20:21:02 +0530

    update permission issue

--- spare_parts/views.py
+++ spare_parts/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Q
-from rest_framework import viewsets, status
+from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
