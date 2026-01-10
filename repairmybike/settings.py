477edddda29efd6be33b2d0be501c3616637e563
mahir <mahir286sharma@gmail.com>
2026-01-11 00:14:34 +0530

    CORS

--- repairmybike/settings.py
+++ repairmybike/settings.py
@@ -307,7 +307,10 @@ CSRF_TRUSTED_ORIGINS = [
     'https://repairmybikebackend-production.up.railway.app',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'http://10.0.2.2:8000'
+    'http://10.0.2.2:8000',
+    # 'https://repairmybike-frontend.vercel.app',
+    'https://repairmybike.in',
+    'https://www.repairmybike.in'
 ]
 
 # Razorpay Configuration
